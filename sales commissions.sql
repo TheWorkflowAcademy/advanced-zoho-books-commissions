14a75fdceea1308fa07762570a6fe22dd8b4c869
Ashton Jensen <67077683+ashtonjensen@users.noreply.github.com>
2022-07-06 22:51:07 +0800

    Update sales commissions.sql
    revised for the course

--- sales commissions.sql
+++ sales commissions.sql
@@ -1,55 +1,23 @@
 SELECT
-		 ip."Invoice ID" 'Invoice ID',
-		 ci."Invoice Number" 'Invoice Number',
-		 ci."Customer" 'Customer',
-		 ci."Sales Rep" 'Rep',
-			CASE
-				 WHEN ci."Sales Rep"  = 'SZN' THEN .03
-				 WHEN ci."Sales Rep"  = 'SZE' THEN .02
-				 ELSE 0.00
-			 END AS 'Rep Commission % ',
-		 (ci."Commissionable %") 'Commissionable % of Payment',
-		 (ci."Commissionable %" / 100) * ip."Amount (BCY)" * (
-			CASE
-				 WHEN ci."Sales Rep"  = 'SZN' THEN .03
-				 WHEN ci."Sales Rep"  = 'SZE' THEN .02
-				 ELSE 0.00
-			 END) 'Commission',
-		 ci."Invoice Total" 'Invoice Total',
-		 ip."Amount (BCY)" 'Amount Paid',
-		 ci."Commissionable Invoice Total" 'Commissionable Invoice Total',
-		 ip."Payment ID" 'Payment ID',
-		 cp."Payment Date" 'Date Paid',
-		 'Invoice' 'Category'
-FROM  "Customer Payments (Zoho Finance)" AS  cp
-JOIN "Invoice Payments (Zoho Finance)" AS  ip ON ip."Payment ID"  = cp."Payment ID" 
-LEFT JOIN "commission_invoices" ci ON ip."Invoice ID"  = ci."Invoice ID"  
+		 c."Payment Date" 'Date Paid',
+		 c."Amount (BCY)" 'Payment Amount',
+		 p."Percent Commissionable" '% Commissionable',
+		 p."Percent Commissionable" * c."Amount (BCY)" * .05 'Commission',
+		 s."Name" 'Salesperson',
+		 'Invoice Payments' 'Category'
+FROM  "Customer Payments (Zoho Books)" c
+JOIN "Invoice Payments (Zoho Books)" i ON i."Payment ID"  = c."Payment ID" 
+JOIN invoice_percentages p ON p."Invoice ID"  = i."Invoice ID" 
+JOIN "Sales Persons (Zoho Books)" s ON p."Salesperson ID"  = s."Sales Person ID"  
 UNION ALL
  SELECT
-		 ci."Invoice ID" 'Invoice ID',
-		 ci."Invoice Number" 'Invoice Number',
-		 ci."Customer" 'Customer',
-		 ci."Sales Rep" 'Rep',
-			CASE
-				 WHEN ci."Sales Rep"  = 'SZN' THEN .03
-				 WHEN ci."Sales Rep"  = 'SZE' THEN .02
-				 ELSE 0.00
-			 END AS 'Rep Commission % ',
-		 (ci."Commissionable %") 'Commissionable % of Payment',
-		 -1 * (ci."Commissionable %" / 100) * cn."Total (BCY)" * (
-			CASE
-				 WHEN ci."Sales Rep"  = 'SZN' THEN .03
-				 WHEN ci."Sales Rep"  = 'SZE' THEN .02
-				 ELSE 0.00
-			 END) 'Commission',
-		 ci."Invoice Total" 'Invoice Total',
-		 cn."Total (BCY)" 'Amount Paid',
-		 ci."Commissionable Invoice Total" 'Commissionable Invoice Total',
-		 cn."CreditNotes ID" 'Payment ID',
-		 cn."Credit Note Date" 'Date Paid',
-		 'Refund' 'Category'
-FROM  "Credit Notes Refund (Zoho Finance)" c
-JOIN "Credit Notes (Zoho Finance)" cn ON cn."CreditNotes ID"  = c."CreditNotes ID" 
-JOIN commission_invoices ci ON ci."Invoice ID"  = cn."Invoice ID"  
-WHERE	 cn."Invoice ID"  is not null
- 
+		 r."Refund Date" 'Date Paid',
+		 r."Amount (BCY)" 'Payment Amount',
+		 p."Percent Commissionable" '% Commissionable',
+		 p."Percent Commissionable" * r."Amount (BCY)" * -.05 'Commission',
+		 s."Name" 'Salesperson',
+		 'Refunds' 'Category'
+FROM  "Credit Notes Refund (Zoho Books)" r
+JOIN "Credit Notes (Zoho Books)" c
+JOIN invoice_percentages p ON p."Invoice ID"  = c."Invoice ID" 
+JOIN "Sales Persons (Zoho Books)" s ON s."Sales Person ID"  = p."Salesperson ID"  
